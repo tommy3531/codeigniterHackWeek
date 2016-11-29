1bf7513e2712e275f5d52f29940655a76e41c690
tommarler <thomas.marler@hotmail.com>
2016-11-29 00:04:19 -0600

    Created a members area, where a user can search by a two digit state and they are given the legislators for that particular state

--- application/controllers/members.php
+++ application/controllers/members.php
@@ -1,10 +1,37 @@
 <?php
 
+require_once '/Users/tommarler/PhpstormProjects/codeigniter/vendor/lobostome/furry-bear/examples/config/config_open_states.php';
+
+
 class Members extends CI_Controller
 {
+
     public function index()
     {
-        $data['main_view'] = 'members/members_view';
+        $data['main_view'] = 'members/member_home';
         $this->load->view('layout/main', $data);
+    }
+
+    public function search()
+    {
+//<?php
+
+
+
+//        try {
+
+            $apiKey = "4a8beb6a33b845edb52173f9f5764b62";
+            $adapter    = new FurryBear\Http\Adapter\Curl();
+            $provider   = new FurryBear\Provider\Source\SunlightOpenStates($adapter, $apiKey);
+            $output     = new FurryBear\Output\Strategy\JsonToArray();
+
+            $fb = new FurryBear\FurryBear();
+            $fb->registerProvider($provider)->registerOutput($output);
+
+            var_dump($fb->legislator_detail->id('DCL000012')->get());
+
+//        } catch (\Exception $e) {
+//            echo $e->getMessage();
+//        }
     }
 }
--- application/views/members/member_home.php
+++ application/views/members/member_home.php
@@ -4,4 +4,48 @@
         <?php echo $this->session->flashdata('login_success') ?>
     </p>
 <?php endif; ?>
+
+<?php $attributes = array('id' => 'legislator_form', 'class'=> 'form_horizontal') ?>
+
+<?php echo validation_errors("<p class='alert alert-danger'>"); ?>
+
+<!-- If the flashdata is set errors are present; show them -->
+<?php if($this->session->flashdata('errors')): ?>
+    <?php echo $this->session->flashdata('errors'); ?>
+
+<?php endif; ?>
+<!-- End if -->
+
+<?php echo form_open('members/search', $attributes); ?>
+
+<!-- Start of the UserState input -->
+<div class="form-group">
+
+    <?php echo form_label('state'); ?>
+    <?php $data = array(
+        'class' => 'form-control',
+        'name' => 'userstate',
+        'placeholder' => 'Enter two digit state (example: If you wanted to search for missouri enter mo'
+
+    );?>
+
+    <?php echo form_input($data); ?>
+
+</div>
+<!-- End of the Username input -->
+
+<!-- Start of the send input -->
+<div class="form-group">
+
+    <?php $data = array(
+        'class' => 'btn btn-primary',
+        'name' => 'submit',
+        'value' => 'send'
+
+    );?>
+
+    <?php echo form_submit($data); ?>
+
+</div>
+<!-- End of the Password input -->
 <h1>Member Area</h1>
